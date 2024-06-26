7c6cc751c46d2b537c2069a610127a930ca2709c
ChristianHauff42 <christian.hauff.42@gmail.com>
2024-06-26 18:53:42 +0200

    remove esc, change super key

--- keymap.c
+++ keymap.c
@@ -40,11 +40,11 @@ const key_override_t **key_overrides = (const key_override_t *[]) {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DVORAK] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,             KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,
+        XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,             KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,
         KC_EQL,  KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,    LGUI(KC_F11), RGUI(KC_F12), KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
         KC_TAB,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_ESC,            KC_RGUI, KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_ENT,
         KC_DEL,  KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,                                KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RCTL,
-        KC_LCTL, XXXXXXX, KC_LGUI, KC_LALT, KC_SPC,           DF(QWERTY),      DF(QWERTY),         KC_BSPC, KC_RALT, KC_RGUI, XXXXXXX, XXXXXXX,
+        KC_LCTL, KC_LGUI, XXXXXXX, KC_LALT, KC_SPC,           DF(QWERTY),      DF(QWERTY),         KC_BSPC, KC_RALT, KC_RGUI, XXXXXXX, XXXXXXX,
                                             KC_LSFT, MO(SYMBOL), MO(MOVE_A),   MO(MOVE_A),MO(SYMBOL), KC_RSFT
     ),
 
@@ -67,11 +67,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [QWERTY] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,           XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PGUP,
+        XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,           XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PGUP,
         KC_BSLS, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    LGUI(KC_F11), RGUI(KC_F12), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PGDN,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ESC,            KC_LGUI, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
         KC_DEL,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RCTL,
-        KC_LCTL, XXXXXXX, KC_LGUI, KC_LALT, KC_SPC,           DF(DVORAK),     DF(DVORAK),          KC_BSPC, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT,
+        KC_LCTL, KC_LGUI, XXXXXXX, KC_LALT, KC_SPC,           DF(DVORAK),     DF(DVORAK),          KC_BSPC, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT,
                                             KC_LSFT, XXXXXXX, MO(MOVE_B),     MO(MOVE_B), XXXXXXX, KC_RSFT
     ),
 
